ec93abd9cf95d6ec007975b70f12bc7b0c112144
Henda Guerbej <irisimprimerie.sahline@gmail.com>
2021-09-15 16:23:49 +0100

    Update Files

--- src/main/java/com/housservice/housstock/service/ClientService.java
+++ src/main/java/com/housservice/housstock/service/ClientService.java
@@ -1,51 +1,17 @@
 package com.housservice.housstock.service;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.housservice.housstock.model.Client;
 import com.housservice.housstock.model.frontend.ClientFrontend;
-import com.housservice.housstock.repository.ClientRepository;
 
-public class ClientService {
+public interface ClientService {
 
-	@Autowired
-	 private ClientRepository clientRepository;
+	public Client createClient(Client client);
 	
-	public Client createClient (Client client)
-	{
-		return clientRepository.save(client);
-	}
-
-
-	 public List< Client > findClientActif() {
-		 
-		 return clientRepository.findClientActif();
-		 // liste des clients frontend 
-	 }
+	public List<Client> findClientActif();
 	
-public List< Client > findClientNotActif() {
-		 
-		 return clientRepository.findClientNotActif();
-
-	 }
-	 
-	public List<ClientFrontend> findClientActif_Houss()
-	{
-		List<Client> listClient = clientRepository.findClientActif();
-		List<ClientFrontend>  listClientFront = new ArrayList<>();
-		
-		for (Client client : listClient) 
-		{
-			ClientFrontend clientFront = new ClientFrontend();
-			// TODO getSecteurById
-			//clientFront.setid
-			listClientFront.add(clientFront);
-		}
-		return listClientFront;
-	}
+	public List<Client> findClientNotActif();
 	
-	
+	public List<ClientFrontend> findClientActif_Houss();
 }
