e3675b700b54856718c9cb185e037483499e4a5f
Henda Gbj <irisimprimerie.sahline@gmail.com>
2023-04-18 09:32:20 +0100

    update code source

---
+++ target/generated-sources/annotations/com/housservice/housstock/mapper/ArticleMapperImpl.java
@@ -0,0 +1,77 @@
+package com.housservice.housstock.mapper;
+
+import com.housservice.housstock.exception.ResourceNotFoundException;
+import com.housservice.housstock.model.Article;
+import com.housservice.housstock.model.EtapeProduction;
+import com.housservice.housstock.model.dto.ArticleDto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-04-14T14:49:58+0100",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
+)
+@Component
+public class ArticleMapperImpl extends ArticleMapper {
+
+    @Override
+    public ArticleDto toArticleDto(Article article) throws ResourceNotFoundException {
+        if ( article == null ) {
+            return null;
+        }
+
+        ArticleDto articleDto = new ArticleDto();
+
+        articleDto.setId( article.getId() );
+        articleDto.setReferenceIris( article.getReferenceIris() );
+        articleDto.setNumFicheTechnique( article.getNumFicheTechnique() );
+        articleDto.setDesignation( article.getDesignation() );
+        List<EtapeProduction> list = article.getEtapeProductions();
+        if ( list != null ) {
+            articleDto.setEtapeProductions( new ArrayList<EtapeProduction>( list ) );
+        }
+        articleDto.setTypeProduit( article.getTypeProduit() );
+        articleDto.setRefClient( article.getRefClient() );
+        articleDto.setRaisonSocial( article.getRaisonSocial() );
+        articleDto.setPrix( article.getPrix() );
+        articleDto.setMiseEnVeille( article.getMiseEnVeille() );
+        articleDto.setPicture( article.getPicture() );
+        articleDto.setDateCreationArticle( article.getDateCreationArticle() );
+
+        updateArticleDto( article, articleDto );
+
+        return articleDto;
+    }
+
+    @Override
+    public Article toArticle(ArticleDto articleDto) throws ResourceNotFoundException {
+        if ( articleDto == null ) {
+            return null;
+        }
+
+        Article article = new Article();
+
+        article.setId( articleDto.getId() );
+        article.setReferenceIris( articleDto.getReferenceIris() );
+        article.setNumFicheTechnique( articleDto.getNumFicheTechnique() );
+        article.setPrix( articleDto.getPrix() );
+        article.setDesignation( articleDto.getDesignation() );
+        article.setTypeProduit( articleDto.getTypeProduit() );
+        article.setRefClient( articleDto.getRefClient() );
+        article.setRaisonSocial( articleDto.getRaisonSocial() );
+        article.setPicture( articleDto.getPicture() );
+        List<EtapeProduction> list = articleDto.getEtapeProductions();
+        if ( list != null ) {
+            article.setEtapeProductions( new ArrayList<EtapeProduction>( list ) );
+        }
+        article.setDateCreationArticle( articleDto.getDateCreationArticle() );
+        article.setMiseEnVeille( articleDto.getMiseEnVeille() );
+
+        updateArticle( articleDto, article );
+
+        return article;
+    }
+}
---
+++ target/generated-sources/annotations/com/housservice/housstock/mapper/NomenclatureMapperImpl.java
@@ -0,0 +1,132 @@
+package com.housservice.housstock.mapper;
+
+import com.housservice.housstock.model.EtapeProduction;
+import com.housservice.housstock.model.Nomenclature;
+import com.housservice.housstock.model.dto.NomenclatureDto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-04-14T14:49:58+0100",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
+)
+@Component
+public class NomenclatureMapperImpl extends NomenclatureMapper {
+
+    @Override
+    public NomenclatureDto toNomenclatureDto(Nomenclature nomenclature) {
+        if ( nomenclature == null ) {
+            return null;
+        }
+
+        NomenclatureDto nomenclatureDto = new NomenclatureDto();
+
+        nomenclatureDto.setId( nomenclature.getId() );
+        nomenclatureDto.setNomNomenclature( nomenclature.getNomNomenclature() );
+        List<String> list = nomenclature.getClientId();
+        if ( list != null ) {
+            nomenclatureDto.setClientId( new ArrayList<String>( list ) );
+        }
+        List<String> list1 = nomenclature.getFournisseurId();
+        if ( list1 != null ) {
+            nomenclatureDto.setFournisseurId( new ArrayList<String>( list1 ) );
+        }
+        nomenclatureDto.setDescription( nomenclature.getDescription() );
+        nomenclatureDto.setType( nomenclature.getType() );
+        nomenclatureDto.setNature( nomenclature.getNature() );
+        List<String> list2 = nomenclature.getChildrensId();
+        if ( list2 != null ) {
+            nomenclatureDto.setChildrensId( new ArrayList<String>( list2 ) );
+        }
+        List<String> list3 = nomenclature.getParentsName();
+        if ( list3 != null ) {
+            nomenclatureDto.setParentsName( new ArrayList<String>( list3 ) );
+        }
+        List<Nomenclature> list4 = nomenclature.getChildrens();
+        if ( list4 != null ) {
+            nomenclatureDto.setChildrens( new ArrayList<Nomenclature>( list4 ) );
+        }
+        List<String> list5 = nomenclature.getChildrensName();
+        if ( list5 != null ) {
+            nomenclatureDto.setChildrensName( new ArrayList<String>( list5 ) );
+        }
+        List<String> list6 = nomenclature.getParentsId();
+        if ( list6 != null ) {
+            nomenclatureDto.setParentsId( new ArrayList<String>( list6 ) );
+        }
+        List<EtapeProduction> list7 = nomenclature.getEtapeProductions();
+        if ( list7 != null ) {
+            nomenclatureDto.setEtapeProductions( new ArrayList<EtapeProduction>( list7 ) );
+        }
+        nomenclatureDto.setCategorie( nomenclature.getCategorie() );
+        nomenclatureDto.setRefIris( nomenclature.getRefIris() );
+        nomenclatureDto.setPicture( nomenclature.getPicture() );
+        nomenclatureDto.setDate( nomenclature.getDate() );
+        nomenclatureDto.setMiseEnVeille( nomenclature.isMiseEnVeille() );
+        nomenclatureDto.setDateMiseEnVeille( nomenclature.getDateMiseEnVeille() );
+
+        updateNomenclatureDto( nomenclature );
+
+        return nomenclatureDto;
+    }
+
+    @Override
+    public Nomenclature toNomenclature(NomenclatureDto nomenclatureDto) {
+        if ( nomenclatureDto == null ) {
+            return null;
+        }
+
+        Nomenclature nomenclature = new Nomenclature();
+
+        nomenclature.setId( nomenclatureDto.getId() );
+        nomenclature.setNomNomenclature( nomenclatureDto.getNomNomenclature() );
+        nomenclature.setDescription( nomenclatureDto.getDescription() );
+        nomenclature.setRefIris( nomenclatureDto.getRefIris() );
+        nomenclature.setType( nomenclatureDto.getType() );
+        nomenclature.setNature( nomenclatureDto.getNature() );
+        nomenclature.setCategorie( nomenclatureDto.getCategorie() );
+        List<String> list = nomenclatureDto.getClientId();
+        if ( list != null ) {
+            nomenclature.setClientId( new ArrayList<String>( list ) );
+        }
+        List<String> list1 = nomenclatureDto.getFournisseurId();
+        if ( list1 != null ) {
+            nomenclature.setFournisseurId( new ArrayList<String>( list1 ) );
+        }
+        List<String> list2 = nomenclatureDto.getChildrensId();
+        if ( list2 != null ) {
+            nomenclature.setChildrensId( new ArrayList<String>( list2 ) );
+        }
+        List<String> list3 = nomenclatureDto.getParentsName();
+        if ( list3 != null ) {
+            nomenclature.setParentsName( new ArrayList<String>( list3 ) );
+        }
+        List<String> list4 = nomenclatureDto.getChildrensName();
+        if ( list4 != null ) {
+            nomenclature.setChildrensName( new ArrayList<String>( list4 ) );
+        }
+        List<String> list5 = nomenclatureDto.getParentsId();
+        if ( list5 != null ) {
+            nomenclature.setParentsId( new ArrayList<String>( list5 ) );
+        }
+        List<Nomenclature> list6 = nomenclatureDto.getChildrens();
+        if ( list6 != null ) {
+            nomenclature.setChildrens( new ArrayList<Nomenclature>( list6 ) );
+        }
+        nomenclature.setPicture( nomenclatureDto.getPicture() );
+        nomenclature.setDate( nomenclatureDto.getDate() );
+        nomenclature.setMiseEnVeille( nomenclatureDto.isMiseEnVeille() );
+        nomenclature.setDateMiseEnVeille( nomenclatureDto.getDateMiseEnVeille() );
+        List<EtapeProduction> list7 = nomenclatureDto.getEtapeProductions();
+        if ( list7 != null ) {
+            nomenclature.setEtapeProductions( new ArrayList<EtapeProduction>( list7 ) );
+        }
+
+        updateNomenclature( nomenclatureDto, nomenclature );
+
+        return nomenclature;
+    }
+}
