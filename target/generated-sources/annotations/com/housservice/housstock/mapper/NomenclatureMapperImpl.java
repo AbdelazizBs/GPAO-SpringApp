b5c25d87fbe00b465b92a3e9f8c14a1ceb92ff5a
Henda Gbj <irisimprimerie.sahline@gmail.com>
2023-04-20 11:08:07 +0100

    update cpde source

--- target/generated-sources/annotations/com/housservice/housstock/mapper/NomenclatureMapperImpl.java
+++ target/generated-sources/annotations/com/housservice/housstock/mapper/NomenclatureMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-04-20T04:06:16+0200",
-    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
+    date = "2023-04-20T11:03:40+0100",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
 )
 @Component
 public class NomenclatureMapperImpl extends NomenclatureMapper {
